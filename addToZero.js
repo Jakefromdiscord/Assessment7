e8e5638946c7830a170a1c76a0b152618ba0a059
Jacob <jacobwamsley12@gmail.com>
2022-02-18 08:19:21 -0700

    AddToZero complete

---
+++ addToZero.js
@@ -0,0 +1,12 @@
+const arr = [1, 4, 11, 2, 37, -4]
+
+let value = 'false'
+for(i = 0; i < arr.length; i++){
+    for(j = 0; j < arr.length; j++){
+        if(arr[i] + arr[j] === 0){
+            value = 'true'
+        }
+    }
+}
+
+console.log(value)
