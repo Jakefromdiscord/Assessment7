c4f0397e4d71042b77d79e5db233beb402388b18
Jacob <jacobwamsley12@gmail.com>
2022-02-18 08:44:59 -0700

    hasUniqueChars complete

---
+++ hasUniqueChars.js
@@ -0,0 +1,15 @@
+let value = false
+
+function Unquie(char){
+for(let i = 0; i < char.length; i++){
+    for(let j = i+1; j < char.length; j++){
+        if(char[i] === char[j]){
+            return value
+        }
+    }
+    }
+    return true
+}
+
+console.log(Unquie('Moon'))
+console.log(Unquie('Jake'))
