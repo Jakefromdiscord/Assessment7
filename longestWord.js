b07adfa78a9f0ed061b9a5d6c912a5c28ca67185
Jacob <jacobwamsley12@gmail.com>
2022-02-18 09:01:10 -0700

    findLongestWord complete

---
+++ longestWord.js
@@ -0,0 +1,14 @@
+let words = ['sam', 'jake', 'cat', 'hello']
+
+const findLongestWord = (string) =>{
+    let word = string.split(' ')
+    let longestWord = 0
+    for(let i = 0; i < word.length; i++){
+        if(word[i].length > longestWord){
+            longestWord = word[i].length
+        }
+    }
+    return longestWord;
+}
+
+console.log(findLongestWord('Jake is Awesome'))
